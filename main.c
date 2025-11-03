7e3cea0f2c31af75e30a89577e19f1795ca8e8c4
PortalGolem <portalgolemmail@gmail.com>
2025-11-03 15:04:09 -0800

    followed tutorial

--- main.c
+++ main.c
@@ -1,26 +1,24 @@
-/*
-  Copyright (C) 1997-2025 Sam Lantinga <slouken@libsdl.org>
-
-  This software is provided 'as-is', without any express or implied
-  warranty.  In no event will the authors be held liable for any damages
-  arising from the use of this software.
-
-  Permission is granted to anyone to use this software for any purpose,
-  including commercial applications, and to alter it and redistribute it
-  freely.
-*/
+#include <SDL3/SDL_error.h>
+#include <SDL3/SDL_log.h>
+#include <SDL3/SDL_surface.h>
+#include <SDL3/SDL_video.h>
+#include <wchar.h>
 #define SDL_MAIN_USE_CALLBACKS 1 /* use the callbacks instead of main() */
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
+#include <SDL3/SDL_render.h>
 
 static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
 
+SDL_Surface *mSurface;
+
 /* This function runs once at startup. */
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
+
   /* Create the window */
-  if (!SDL_CreateWindowAndRenderer("Hello World", 800, 600,
-                                   SDL_WINDOW_FULLSCREEN, &window, &renderer)) {
+  if (!SDL_CreateWindowAndRenderer("Hydrogen Simulator", 800, 600,
+                                   SDL_WINDOW_RESIZABLE, &window, &renderer)) {
     SDL_Log("Couldn't create window and renderer: %s", SDL_GetError());
     return SDL_APP_FAILURE;
   }
@@ -29,7 +27,7 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
 
 /* This function runs when a new event (mouse input, keypresses, etc) occurs. */
 SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
-  if (event->type == SDL_EVENT_KEY_DOWN || event->type == SDL_EVENT_QUIT) {
+  if (event->type == SDL_EVENT_QUIT) {
     return SDL_APP_SUCCESS; /* end the program, reporting success to the OS. */
   }
   return SDL_APP_CONTINUE;
@@ -37,25 +35,9 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
 
 /* This function runs once per frame, and is the heart of the program. */
 SDL_AppResult SDL_AppIterate(void *appstate) {
-  const char *message = "Poop. He He.";
-  int w = 0, h = 0;
-  float x, y;
-  const float scale = 4.0f;
-
-  /* Center the message and scale it up */
-  SDL_GetRenderOutputSize(renderer, &w, &h);
-  SDL_SetRenderScale(renderer, scale, scale);
-  x = ((w / scale) - SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE * SDL_strlen(message)) /
-      2;
-  y = ((h / scale) - SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE) / 2;
-
-  /* Draw the message */
-  SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+  SDL_SetRenderDrawColor(renderer, 100, 149, 237, 255);
   SDL_RenderClear(renderer);
-  SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-  SDL_RenderDebugText(renderer, x, y, message);
   SDL_RenderPresent(renderer);
-
   return SDL_APP_CONTINUE;
 }
 
